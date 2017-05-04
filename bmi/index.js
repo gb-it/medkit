89785070cf20e904f6b6791084117e6ea1446e50
Sauerbrei <soeren.sauerbrei@outlook.com>
2017-05-04 16:05:06 +0200

    Encapsulated and Added getRangeTable()

--- bmi/index.js
+++ bmi/index.js
@@ -50,6 +50,122 @@ var _ = {};
 _.each = require('lodash.foreach'),
 _.assign = require('lodash.assign');
 
+var defaultRange = {
+	0: '16',
+	1: '16-17',
+	2: '17-18.5',
+	3: '18.5-25',
+	4: '25-30',
+	5: '30-35',
+	6: '35-40',
+	7: '40'
+};
+
+var specificRanges = {
+	male: {
+		age: {
+			'16-16': {
+				2: '17-18',
+				3: '18-24',
+				4: '24-28',
+				5: '28-35'
+			},
+			'17-24': {
+				2: '17-20',
+				3: '20-25'
+			},
+			'25-34': {
+				2: '17-21',
+				3: '21-26',
+				4: '26-30'
+			},
+			'35-44': {
+				2: '17-22',
+				3: '22-28',
+				4: '28-31',
+				5: '31-35'
+			},
+			'45-54': {
+				2: '17-23',
+				3: '23-29',
+				4: '29-33',
+				5: '33-35'
+			},
+			'55-64': {
+				2: '17-24',
+				3: '24-30',
+				4: '30-34',
+				5: '34-35'
+			},
+			'64-999': {
+				2: '17-25',
+				3: '25-30',
+				4: '30-35',
+				5: '35-37',
+				6: '37-40'
+			}
+		},
+		2: '17-20',
+		3: '20-25'
+	},
+
+	female: {
+		age: {
+			'16-24': {
+				2: '17-18',
+				3: '18-24',
+				4: '24-28',
+				5: '28-35'
+			},
+			'25-34': {
+				2: '17-19',
+				3: '19-25',
+				4: '25-30'
+			},
+			'35-44': {
+				2: '17-20',
+				3: '20-26',
+				4: '26-31',
+				5: '31-35'
+			},
+			'45-54': {
+				2: '17-21',
+				3: '21-27',
+				4: '27-32',
+				5: '32-35'
+			},
+			'55-64': {
+				2: '17-22',
+				3: '22-28',
+				4: '28-33',
+				5: '33-35'
+			},
+			'64-999': {
+				2: '17-23',
+				3: '23-29',
+				4: '29-34',
+				5: '34-37',
+				6: '37-40'
+			}
+		},
+		2: '17-19',
+		3: '19-24',
+		4: '24-30'
+	}
+};
+
+var defaultMessages = {
+	0: 'Very severely underweight',
+	1: 'Severely underweight',
+	2: 'Underweight',
+	3: 'Healthy',
+	4: 'Overweight',
+	5: 'Obese Class I',
+	6: 'Obese Class II',
+	7: 'Obese Class III'
+};
+
+
 /**
  * 
  * @param {integer} height
@@ -68,7 +184,7 @@ function BMI(height , mass , usePounds) {
 	this.measurement = '';
 	
 	this.usePounds(usePounds);
-	return this.calc();
+	return this;
 };
 
 /**
@@ -78,7 +194,7 @@ function BMI(height , mass , usePounds) {
  */
 BMI.prototype.setHeight = function(height) {
 	this.height = height;
-	return this.calc();
+	return this;
 };
 
 
@@ -89,7 +205,7 @@ BMI.prototype.setHeight = function(height) {
  */
 BMI.prototype.usePounds = function(usePounds) {
 	this.measurement = (usePounds === true ? 'pounds/inches' : 'kilograms/metres');
-	return this.calc();
+	return this;
 };
 
 
@@ -100,7 +216,7 @@ BMI.prototype.usePounds = function(usePounds) {
  */
 BMI.prototype.setMass = function(mass) {
 	this.mass = mass;
-	return this.calc();
+	return this;
 };
 
 
@@ -111,7 +227,7 @@ BMI.prototype.setMass = function(mass) {
  */
 BMI.prototype.setAge = function(age) {
 	this.age = age;
-	return this.calc();
+	return this;
 };
 
 /**
@@ -134,159 +250,22 @@ BMI.prototype.setGender = function(gender) {
 		this.gender = 'unknown';
 	}
 	
-	return this.calc();
+	return this;
 };
 		
 /**
- * 
+ * Calculates 
  * @returns {BMI}
  */
 BMI.prototype.calc = function() {
-	this.index = (this.measurement === 'pounds/inches' ? 
-					(this.mass / this.height / this.height) * 703 : 
-					(this.mass / Math.pow(this.height/100,2))
-	).toFixed(1);
-
-	var defaultIndexes = {
-		0: '16',
-		1: '16-17',
-		2: '17-18.5',
-		3: '18.5-25',
-		4: '25-30',
-		5: '30-35',
-		6: '35-40',
-		7: '40'
-	};
+	// calculating the BMI
+	this.index = getBMIValue(this);
 	
-	var specificIndexes = {
-		male: {
-			age: {
-				'16-16': {
-					2: '17-18',
-					3: '18-24',
-					4: '24-28',
-					5: '28-35'
-				},
-				'17-24': {
-					2: '17-20',
-					3: '20-25'
-				},
-				'25-34': {
-					2: '17-21',
-					3: '21-26',
-					4: '26-30'
-				},
-				'35-44': {
-					2: '17-22',
-					3: '22-28',
-					4: '28-31',
-					5: '31-35'
-				},
-				'45-54': {
-					2: '17-23',
-					3: '23-29',
-					4: '29-33',
-					5: '33-35'
-				},
-				'55-64': {
-					2: '17-24',
-					3: '24-30',
-					4: '30-34',
-					5: '34-35'
-				},
-				'64-999': {
-					2: '17-25',
-					3: '25-30',
-					4: '30-35',
-					5: '35-37',
-					6: '37-40'
-				}
-			},
-			2: '17-20',
-			3: '20-25'
-		},
-		
-		female: {
-			age: {
-				'16-24': {
-					2: '17-18',
-					3: '18-24',
-					4: '24-28',
-					5: '28-35'
-				},
-				'25-34': {
-					2: '17-19',
-					3: '19-25',
-					4: '25-30'
-				},
-				'35-44': {
-					2: '17-20',
-					3: '20-26',
-					4: '26-31',
-					5: '31-35'
-				},
-				'45-54': {
-					2: '17-21',
-					3: '21-27',
-					4: '27-32',
-					5: '32-35'
-				},
-				'55-64': {
-					2: '17-22',
-					3: '22-28',
-					4: '28-33',
-					5: '33-35'
-				},
-				'64-999': {
-					2: '17-23',
-					3: '23-29',
-					4: '29-34',
-					5: '34-37',
-					6: '37-40'
-				}
-			},
-			2: '17-19',
-			3: '19-24',
-			4: '24-30'
-		}
-	};
+	// calculating the specific Range-Table
+	var thisRange = getRange(this);
 	
-	
-	// identify the specific indexes
-	// if the gender is known
-	var identifiedIndex = {};
-	
-	if (this.gender !== 'unknown') {
-		if (this.age >= 16) {
-			_.each(specificIndexes[this.gender].age, function(v,k) {
-				var ageRange = k.split('-').map(function(val) { return parseInt(val); });
-				if (this.age >= ageRange[0] && this.age <= ageRange[1]) {
-					identifiedIndex = v;
-				}
-			}.bind(this));			
-		} else {
-			var copyMaleObj = specificIndexes[this.gender];
-			delete copyMaleObj.age;
-			identifiedIndex = copyMaleObj;
-		}
-	}
-	
-	// Now overwrite the default indexes with the specific ones
-	_.assign(defaultIndexes, identifiedIndex);
-	
-	
-	// generating the Error Messages
-	var defaultMessages = {
-		0: 'Very severely underweight',
-		1: 'Severely underweight',
-		2: 'Underweight',
-		3: 'Healthy',
-		4: 'Overweight',
-		5: 'Obese Class I',
-		6: 'Obese Class II',
-		7: 'Obese Class III'
-	};
-	_.each(defaultIndexes, function(v,k) {
+	// updating the Result
+	_.each(thisRange, function(v,k) {
 		var indexes = v.split('-').map(function(val) { return parseInt(val); });
 		if (k === '0') {
 			if (this.index < indexes[0]) {
@@ -307,6 +286,17 @@ BMI.prototype.calc = function() {
 };
 
 
+/**
+ * 
+ * @returns {RangeTable}
+ */
+BMI.prototype.getRangeTable = function() {
+	var thisRange = getRange(this);
+	return mapMessagesToRanges(thisRange);
+};
+
+
+
 
 
 /**
@@ -317,6 +307,63 @@ BMI.prototype.calc = function() {
  */
 function constructor(height, mass) {
 	return new BMI(height, mass);
+}
+
+function getBMIValue(BMIObj) {
+	return (BMIObj.measurement === 'pounds/inches' ? 
+					(BMIObj.mass / BMIObj.height / BMIObj.height) * 703 : 
+					(BMIObj.mass / Math.pow(BMIObj.height/100,2))
+	).toFixed(1);
+}
+
+/**
+ * Creates a Table with Messages as Key
+ * @param {Ranges} rangeObject
+ * @returns {RangeTable}
+ */
+function mapMessagesToRanges(rangeObject) {
+	_.each(rangeObject, function(v,k) {
+		rangeObject[defaultMessages[k]] = v;
+		delete rangeObject[k];
+	});
+	return rangeObject;
+}
+
+/**
+ * identify the specific indexes,
+ * if the gender is known
+ * 
+ * @param {BMI} BMIObject
+ * @returns {Range|null} Returns the Range which will overwrite the defaultRange
+ */
+function identifyRange(BMIObject) {
+	var identifiedRange = {};
+	
+	if (BMIObject.gender !== 'unknown') {
+		if (BMIObject.age >= 16) {
+			_.each(specificRanges[BMIObject.gender].age, function(v,k) {
+				var ageRange = k.split('-').map(function(val) { return parseInt(val); });
+				if (BMIObject.age >= ageRange[0] && BMIObject.age <= ageRange[1]) {
+					identifiedRange = v;
+				}
+			});			
+		} else {
+			var copyMaleObj = specificRanges[BMIObject.gender];
+			delete copyMaleObj.age;
+			identifiedRange = copyMaleObj;
+		}
+	}
+	return identifiedRange;
+}
+
+/**
+ * Merges DefaultRanges with Patient-specific-Ranges
+ * and returns the result
+ * @param {BMI} BMIObject
+ * @returns {unresolved}
+ */
+function getRange(BMIObject) {
+	return _.assign(defaultRange, identifyRange(BMIObject));
 }
 
 
--- test/bmi.js
+++ test/bmi.js
@@ -6,6 +6,9 @@ describe('bmi', function() {
 	it('(180,80).index should return 24.7', function() {
 		assert.equal(bmi(180,80).index, 24.7);
 	});
+	it('(180,80).message should return Healthy', function() {
+		assert.equal(bmi(180,80).message, 'Healthy');
+	});
 	it('(180,80).setGender("m").setAge(55).message should return Healthy', function() {
 		assert.equal(bmi(180,80).setGender("m").setAge(55).message, 'Healthy');
 	});
